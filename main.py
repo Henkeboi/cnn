6097b0b98a363f9a14641da665eb6d450e29cab3
Henrik <hgg1998@gmail.com>
2021-02-23 23:13:04 +0100

    Add sigmoid.

--- main.py
+++ main.py
@@ -5,21 +5,24 @@ import numpy as np
 
 def main():
     relu = utility.Relu()
-    vector_relu_d = utility.VectorDerivativeRelu()
-    matrix_relu_d = utility.MatrixDerivativeRelu()
+    relu_d = utility.MatrixDerivativeRelu()
+
+    sigmoid = utility.Sigmoid()
+    sigmoid_d = utility.DerivativeSigmoid()
 
     data = np.random.rand(5, 5).flatten().reshape(1, 25)
     label = np.random.rand(1, 3)
 
     input_layer = layers.InputLayer(25, 5)
-    hidden_layer0 = layers.HiddenLayer(5, 4, relu, matrix_relu_d)
-    hidden_layer1 = layers.HiddenLayer(4, 3, relu, matrix_relu_d)
-    output_layer = layers.HiddenLayer(3, 3, relu, matrix_relu_d)
+    hidden_layer0 = layers.HiddenLayer(5, 4, relu, relu_d)
+    hidden_layer1 = layers.HiddenLayer(4, 3, relu, relu_d)
+    output_layer = layers.HiddenLayer(3, 3, sigmoid, relu_d)
     hidden_layers = [hidden_layer0, hidden_layer1, output_layer]
 
     nn = network.Network(input_layer, hidden_layers)
     for i in range(10000):
         print(nn.train(label, data).sum())
+        #nn.train(label, data).sum()
 
 
 
--- utility.py
+++ utility.py
@@ -1,9 +1,23 @@
 import numpy as np
+import math
 
 class Sigmoid:
     def __call__(self, activation):
-        return 1
-    
+        for i in range(activation.shape[1]):
+            activation[0][i] = 1.0 / (1.0 + math.exp(-1.0 * activation[0][i]))
+        return activation 
+
+class DerivativeSigmoid:
+    def __call__(self, activation):
+        size = activation.shape[0]
+        a_d = np.full((size, size), None)
+        for i in range(activation.shape[0]):
+            for j in range(activation.shape[0]):
+                a_d[i][j] = 1 * (activation.item(i, 0))
+        return a_d
+
+
+
 class Relu:
     def __call__(self, activation):
         return activation * (activation > 0)
@@ -14,7 +28,7 @@ class MatrixDerivativeRelu:
         a_d = np.full((size, size), None)
         for i in range(activation.shape[0]):
             for j in range(activation.shape[0]):
-                a_d[i][j] = 1 * (activation.item(i, 0) >= 0)
+                a_d[i][j] = 1.0 * (activation.item(i, 0) >= 0)
         return a_d
 
 class VectorDerivativeRelu:
