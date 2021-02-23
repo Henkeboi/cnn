5cc27e250eb93178a069e9a59c11427fa702a2ed
Henrik <hgg1998@gmail.com>
2021-02-23 22:11:12 +0100

    Add tree classes.

--- main.py
+++ main.py
@@ -9,17 +9,17 @@ def main():
     matrix_relu_d = utility.MatrixDerivativeRelu()
 
     data = np.random.rand(5, 5).flatten().reshape(1, 25)
-    label = np.random.rand(1, 1)
+    label = np.random.rand(1, 3)
 
     input_layer = layers.InputLayer(25, 5)
     hidden_layer0 = layers.HiddenLayer(5, 4, relu, matrix_relu_d)
     hidden_layer1 = layers.HiddenLayer(4, 3, relu, matrix_relu_d)
-    output_layer = layers.HiddenLayer(3, 1, relu, matrix_relu_d)
+    output_layer = layers.HiddenLayer(3, 3, relu, matrix_relu_d)
     hidden_layers = [hidden_layer0, hidden_layer1, output_layer]
 
     nn = network.Network(input_layer, hidden_layers)
-    for i in range(1020):
-        print(nn.train(label, data))
+    for i in range(10000):
+        print(nn.train(label, data).sum())
 
 
 
--- network.py
+++ network.py
@@ -25,17 +25,12 @@ class Network:
 
         f = self._hidden_layers[-1].derivative(transfers[-1].T)
         a = self._hidden_layers[-1].derivative(activations[-1].T)
-        deltas.append(f @ self._hidden_layers[-1].derivative(activations[-1]) @ loss)
+        deltas.append(f @ a @ loss.T)
 
         for i, layer in enumerate(reversed(self._hidden_layers)):
             w_d.append(activations[-2 - i].T @ deltas[-1].T)
             a_d = layer.derivative(activations[-2 - i].T)
             w = layer._weights
-
-            # print(a_d.shape)
-            # print(w.shape)
-            # print(deltas[-1].shape)
-
             delta = a_d.T @ layer._weights @ deltas[-1]
             deltas.append(delta)
 
@@ -47,7 +42,7 @@ class Network:
 
 
     def train(self, label, data):
-        la = 0.0001
+        la = 0.000001
         w_d, loss = self.backward_pass(label, data)
         for i, layer in enumerate(reversed(self._hidden_layers)):
             layer._weights = layer._weights + la * w_d[i]
